a3e1bfb8d3c09e519059f12d767c61fd54b10c54
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-09-03 16:35:06 +0900

    signup

---
+++ src/main/java/com/sparta/miniproject2/dto/UserRequestDto.java
@@ -0,0 +1,28 @@
+package com.sparta.miniproject2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDto {
+
+    @Pattern(regexp = "[a-zA-Z\\d]*${3,12}")
+    @Size(min = 4, max = 12)
+    @NotBlank
+    private String username;
+
+    @Pattern(regexp = "[a-zA-Z\\d]*${3,32}")
+    @Size(min = 4, max = 32)
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String passwordConfirm;
+}
